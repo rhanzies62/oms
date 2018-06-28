cc0820dcc7e1be8e95d8722d97938e4dc4786472
keithvancapiral <keithvancapiral0406@gmail.com>
2018-06-29 01:25:04 +0800

    Configure  cs Project

--- OMS.Core/Mapper/UserProfile.cs
+++ OMS.Core/Mapper/UserProfile.cs
@@ -6,13 +6,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO = OMS.Core.DTO;
+using Entities = OMS.Core.Entities;
 
 namespace OMS.Core.Mapper
 {
     public class UserProfile : Profile
     {
         public UserProfile() {
-            CreateMap<UserDTO, User>();
+            //CreateMap<DTO.se, User>();
 
             
         }
