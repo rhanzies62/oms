131daf8eebfb58624bfb378642fd3192267bf0f6
keithvancapiral <keithvancapiral0406@gmail.com>
2018-06-29 01:09:29 +0800

    AddAdminVIew

---
+++ OMS.Web/Controllers/TopanAdminController.cs
@@ -0,0 +1,71 @@
+﻿using System.Web.Mvc;
+
+namespace OMS.Web.Controllers
+{
+    public class TopanAdminController : Controller
+    {
+        // GET: TopanAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Variants()
+        {
+            return View();
+        }
+
+        public ActionResult CreateVariants()
+        {
+            return View();
+        }
+
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult CreateProducts()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Employees()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEmployees()
+        {
+            return View();
+        }
+
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        public ActionResult CreateAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult Orders()
+        {
+            return View();
+        }
+
+        public ActionResult Transactions()
+        {
+            return View();
+        }
+
+
+
+    }
+}
