37ed8ccb063f28ff29f7190de80fdf477433a21e
lorenz pascual <lorenzpascual45@gmail.com>
2018-07-01 23:20:18 +0800

    add userservice and dto

---
+++ OMS.Core/DTO/User.cs
@@ -0,0 +1,50 @@
+﻿using OMS.Core.Interface.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Core.DTO
+{
+    public class User : IAudit
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required, StringLength(25)]
+        public string FirstName { get; set; }
+        [Required, StringLength(25)]
+        public string LastName { get; set; }
+        [Required]
+        public int AddressID { get; set; }
+        [Required]
+        public int MobileNumber { get; set; }
+        [Required, StringLength(25)]
+        public string Email { get; set; }
+        [Required]
+        public Entities.Gender Gender { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+
+        //public Address Address { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public string UpdatedBy { get; set; }
+        [Required]
+        public DateTime UpdatedDate { get; set; }
+
+        [Required]
+        public int AccountID { get; set; }
+
+        //public Account Account { get; set; }
+        //public IEnumerable<Order> Order { get; set; }
+        //public IEnumerable<Transaction> Transaction { get; set; }
+
+
+    }
+}
---
+++ OMS.Service/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using OMS.Core.Interface.Services;
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities = OMS.Core.Entities;
+using OMS.Core.Interface.Repositories;
+using DTO = OMS.Core.DTO;
+namespace OMS.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private ICRUDRepository<DTO.User> _userRepo;
+        public UserService(ICRUDRepository<DTO.User> userRepo)
+        {
+            _userRepo = userRepo;
+        }
+        public DTO.Response<DTO.User> CreateUser(DTO.User User)
+        {
+            DTO.Response<DTO.User> user = new DTO.Response<DTO.User>();
+            try {
+                _userRepo.Add(User);
+                user.Success = true;
+                user.Data = User;
+            }
+            catch (Exception e) {
+                user.ErrorMessage = e.Message;
+                user.Success = false;
+            }
+            return user;
+        }
+
+        public DTO.User GetUser(int userID, bool isActive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DTO.User> ListUsers(bool isActive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTO.Response<DTO.User> RemoveUser(int userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTO.Response<DTO.User> UpdateUser(DTO.User User)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
