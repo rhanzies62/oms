a961a18f807c90ce0f3a7046f643afcbf2a25c33
lorenz pascual <lorenzpascual45@gmail.com>
2018-07-05 11:30:13 +0800

    add variant service

---
+++ OMS.Core/Interface/Services/IVariantService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OMS.Core.DTO;
+
+namespace OMS.Core.Interface.Services
+{
+    public interface IVariantService
+    {
+        IEnumerable<Variant> ListVariants();
+
+        Response<Variant> CreateVariant(Variant Variant);
+
+        Response<Variant> UpdateVariant(Variant Variant);
+
+        Response<Variant> RemoveVariant(int variantID);
+
+        Variant GetVariantByVariantID(int variantID);
+
+    }
+}
