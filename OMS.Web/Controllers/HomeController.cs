9a34e0c1b90d7cb06e228d13597cbfbfa2526e45
keithvancapiral <keithvancapiral0406@gmail.com>
2018-07-11 16:31:07 +0800

    Admin Ui Fixed

--- OMS.Web/Controllers/HomeController.cs
+++ OMS.Web/Controllers/HomeController.cs
@@ -38,36 +38,10 @@ namespace OMS.Web.Controllers
 
         public ActionResult ProductList()
         {
-            Product Prod = new Product();
-            Prod.Name = "1 X 2 x 8ft";
-            Prod.Description = "Plywood";
-            Prod.Price = 67;
-            Prod.CreatedBy = "Keith Van Capiral";
-            Prod.CreatedDate = DateTime.UtcNow;
-            Prod.UpdatedBy = "Keith";
-            Prod.UpdatedDate = DateTime.UtcNow;
-
-            Product Prod1 = new Product();
-            Prod1.Name = "1 X 2 x 10ft";
-            Prod1.Description = "Plywood";
-            Prod1.Price = 67;
-            Prod1.CreatedBy = "Keith Van Capiral";
-            Prod1.CreatedDate = DateTime.UtcNow;
-            Prod1.UpdatedBy = "Keith";
-            Prod1.UpdatedDate = DateTime.UtcNow;
 
 
 
-
-
-            List<Product> Products = new List<Product>();
-            Products.Add(Prod);
-            Products.Add(Prod1);
-
-
-
-
-            return View(Products);
+            return View();
         }
 
 
--- OMS.Web/Scripts/init.js
+++ OMS.Web/Scripts/init.js
@@ -1,8 +1,7 @@
-(function($){
-  $(function(){
+
+$(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+});
